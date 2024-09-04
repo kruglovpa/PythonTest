bf66c32778cf568080f45cb28a8e9d48e89b8e07
kruglovpa <kruglov.pa@mail.ru>
2024-09-04 17:59:14 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# динамическая типизация
+name = 'Pavel'
+print ('Name:',(name))
+age = 50
+print ('Age:',(age))
+age = (age) +10
+print ('New Age:',(age))
+is_student = True
+print ('Is Student:',(is_student))
