2ff3d4b871cb629a7f68bab64ca8ff19795b6752
kruglovpa <kruglov.pa@mail.ru>
2024-09-05 17:50:17 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,7 @@
 # Кортежи и списки
 immutable_var = tuple ['Шуфутинский', 3, 'сентября']
-print ('Кортеж:', (immutable_var))
+print ('Кортеж:', immutable_var)
 #immutable_var [1] =5
-#print ('Кортеж:', (immutable_var))
+#print ('Кортеж:', immutable_var)
 mutable_list = [5, 'сентября', 2024, 'года']
-print ('Список:', (mutable_list))
+print ('Список:', mutable_list)
