f2f31fb516163826c1a663292aa937367c52b8f0
kruglovpa <kruglov.pa@mail.ru>
2024-09-05 17:05:24 +0300

    Delete module_1_3.py

--- module_1_3.py
+++
@@ -1,9 +0,0 @@
-# динамическая типизация
-name = 'Pavel'
-print ('Name:',(name))
-age = 50
-print ('Age:',(age))
-age = (age) +10
-print ('New Age:',(age))
-is_student = True
-print ('Is Student:',(is_student))
