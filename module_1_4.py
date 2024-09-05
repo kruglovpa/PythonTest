3f4af2f24e2fe640fd6619b7d23ed2f2012125af
kruglovpa <kruglov.pa@mail.ru>
2024-09-05 17:05:34 +0300

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,9 +0,0 @@
-# организация программ и методы строк
-my_string = input ('Введите любой текст ')
-count = len (my_string)
-print ('В вашем тесте ', int (count), 'символов')
-print ('ваш текст в верхнем регистре: ', my_string .upper ())
-print ('ваш текст в нижнем регистре: ', my_string .lower ())
-print ('ваш текст без пробелов: ', my_string .replace (' ', ''))
-print ('первый символ строки ', my_string [0])
-print ('последний символ строки ', my_string [-1])
