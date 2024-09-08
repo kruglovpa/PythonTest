678af5700559d8d73f4aac07499d8469eb4c2e3d
kruglovpa <kruglov.pa@mail.ru>
2024-09-08 12:13:00 +0300

    Delete module_1_5.py

--- module_1_5.py
+++
@@ -1,7 +0,0 @@
-# Кортежи и списки
-immutable_var = tuple ['Шуфутинский', 3, 'сентября']
-print ('Кортеж:', immutable_var)
-#immutable_var [1] =5
-#print ('Кортеж:', (immutable_var))
-mutable_list = [5, 'сентября', 2024, 'года']
-print ('Список:', mutable_list)
