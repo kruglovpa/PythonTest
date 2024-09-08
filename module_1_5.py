3535429d87e0a1c24a6f6d7cfad25917f6e5a10e
kruglovpa <kruglov.pa@mail.ru>
2024-09-08 12:11:53 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -2,6 +2,6 @@
 immutable_var = tuple ['Шуфутинский', 3, 'сентября']
 print ('Кортеж:', immutable_var)
 #immutable_var [1] =5
-#print ('Кортеж:', immutable_var)
+#print ('Кортеж:', (immutable_var))
 mutable_list = [5, 'сентября', 2024, 'года']
 print ('Список:', mutable_list)
