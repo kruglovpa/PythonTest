4bc7e77102ab8bc13a403793e69efe99b158c4dc
kruglovpa <kruglov.pa@mail.ru>
2024-09-17 13:39:00 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -3,15 +3,13 @@ calls = 0
 
 
 def count_calls():
-#    global calls
-#    calls += 1
-    print(calls)
+    global calls
+    calls += 1
 
 
 def string_info(string):
     print(tuple([len(string), string.upper(), string.lower()]))
-    global calls
-    calls += 1
+    count_calls()
 
 
 def is_contains(string, list_to_search):
@@ -20,8 +18,7 @@ def is_contains(string, list_to_search):
         i = i.lower()
         lower_list.append(i)
     print(string.lower() in lower_list)
-    global calls
-    calls += 1
+    count_calls()
 
 
 string_info('Capybara')
@@ -30,4 +27,4 @@ string_info('Armageddon')
 is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])
 is_contains('cycle', ['recycling', 'cyclic'])
 
-count_calls()
+print(calls)
