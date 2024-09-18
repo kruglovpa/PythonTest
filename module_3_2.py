a2cda9ea8930115f79ff09928da8e1d343b70cf8
Pavel <kruglov.pa@mail.ru>
2024-09-18 18:46:26 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,19 +1,22 @@
 # Способы вызова функции
 def send_email(message, recipient, sender='university.help@gmail.com'):
-    domain = ['.com', '.ru', '.net']
+    true_adres = False
+
     if '@' in recipient and '@' in sender:
-        print('@ Ok')
-    for i in domain:
-        if domain[i] in recipient:
-            print('domain recipient Ok')
-    if domain[i] in sender:
-        print('domain sender Ok')
-    else:
+        if recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'):
+            if sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'):
+                true_adres = True
+    if true_adres == False:
         print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
-    # elif sender == recipient:
-    #     print('Нельзя отправить письмо самому себе!')
-    # else:
-    #     print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+    if true_adres == True and sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    if true_adres == True and sender != recipient and sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+    if true_adres == True and sender != recipient and sender != 'university.help@gmail.com':
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
