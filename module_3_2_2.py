01bc102905c073e18efa0c971854e96b139215be
Pavel <kruglov.pa@mail.ru>
2024-09-18 22:58:30 +0300

    Add files via upload

--- module_3_2_2.py
+++ module_3_2_2.py
@@ -3,7 +3,7 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
     domain_list = ['.com', '.ru', '.net']
     true_domain1 = False
     true_domain2 = False
-    true_adres = False
+    true_ad = False
 
     for i in domain_list:
         if recipient.endswith(i):
@@ -13,18 +13,18 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
         if sender.endswith(i):
             true_domain2 = True
             break
-    if '@' in recipient and '@' in sender and true_domain1 == true_domain2 == True:
-        true_adres = True
+    if '@' in recipient and '@' in sender:
+        true_ad = True
 
-    if true_adres == False:
-        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
-    else:
+    if true_domain1 == true_domain2 == true_ad == True:
         if sender == recipient:
             print('Нельзя отправить письмо самому себе!')
         elif sender == 'university.help@gmail.com':
             print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
         else:
             print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+    else:
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
