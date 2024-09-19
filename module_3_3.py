662f01f10f70a64e8daafa3e0fedf253d37b836b
Pavel <kruglov.pa@mail.ru>
2024-09-19 10:22:15 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -6,4 +6,13 @@ value_list = [2, 'строка', True]
 value_dict = {'a': 1, 'b': 'строка', 'c': True}
 value_list2 = [54.32, 'Строка']
 
+print_params()
+print_params(5)
+print_params(5, 8)
+print_params(False, 5, 'проверка')
+print_params(b = 25)
+print_params(c = [1,2,3])
+print_params(*value_list)
+print_params(**value_dict)
+print_params(*value_list2)
 print_params(*value_list2, 42)
