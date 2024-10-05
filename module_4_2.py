6eb5c192da10e53c4088aa307cb66914915dff60
Pavel <kruglov.pa@mail.ru>
2024-10-06 02:10:08 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,11 @@
+# Пространство имен
+def test_function():
+    def inner_function():
+        x = 'Я в области видимости функции test_function'
+        print(x)
+
+    inner_function()
+
+
+test_function()
+# inner_function()
