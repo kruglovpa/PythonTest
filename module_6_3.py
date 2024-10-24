fc8e3db5acd00405754affaf2c961d520d748462
Pavel <kruglov.pa@mail.ru>
2024-10-25 01:44:00 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,41 +1,44 @@
 # Множественное наследование
 class Horse:
-    def __ini__(self, x_distance=0, sound='Frrr'):
-        self.x_distance = x_distance
-        self.sound = sound
+    x_distance = 0
+
+    def __init__(self):
+        self.sound = 'Frrr'
 
     def run(self, dx):
-        x_distance += dx
-        return x_distance
+        self.x_distance += dx
 
 
 class Eagle:
-    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+    y_distance = 0
+
+    def __init__(self):
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
-        y_distance += dy
-        return y_distance
+        self.y_distance += dy
+
 
 class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
     def move(self, dx, dy):
-        super.run()
-        super.fly()
+        super().run(dx)
+        super().fly(dy)
 
     def get_pos(self):
-        distance = [super.x_distance, super.y_distance]
-        return distance
+        return self.x_distance, self.y_distance
 
     def voice(self):
         print(self.sound)
 
-p1 = Pegasus()
 
+p1 = Pegasus()
 print(p1.get_pos())
 p1.move(10, 15)
 print(p1.get_pos())
 p1.move(-5, 20)
 print(p1.get_pos())
-
 p1.voice()
