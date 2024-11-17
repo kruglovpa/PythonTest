042fbf5b2fa4fe4eed9c89ea6f86a78c03b5e4d0
Pavel <kruglov.pa@mail.ru>
2024-11-17 22:10:03 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,12 @@
+# Генераторные сборки
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+# print(list(zip(first, second)))
+first_result = (len(i[0]) - len(i[1]) for i in zip(first, second) if len(i[0]) != len(i[1]))
+# print(list(len(i) for i in first), list(len(i) for i in second))
+second_result = ((len(i) for i in first) == (len(i) for i in second) for i in range(len(first)))
+
+if __name__ == '__main__':
+    print(list(first_result))
+    print(list(second_result))
