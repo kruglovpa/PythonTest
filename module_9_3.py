ae59708f03114ff756036f3736a313ceab683f8f
Pavel <kruglov.pa@mail.ru>
2024-11-17 22:41:19 +0300

    Add files via upload

--- module_9_3.py
+++ module_9_3.py
@@ -4,8 +4,10 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 # print(list(zip(first, second)))
 first_result = (len(i[0]) - len(i[1]) for i in zip(first, second) if len(i[0]) != len(i[1]))
+# second_result = (len(i[0]) == len(i[1]) for i in zip(first, second))
+
 # print(list(len(i) for i in first), list(len(i) for i in second))
-second_result = ((len(i) for i in first) == (len(i) for i in second) for i in range(len(first)))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
 if __name__ == '__main__':
     print(list(first_result))
