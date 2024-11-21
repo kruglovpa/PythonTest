8b9f502f260ea5a33e71d42bcc91d602632c7457
Pavel <kruglov.pa@mail.ru>
2024-11-21 22:20:58 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,11 @@
+# Генераторы
+def all_variants(text):
+   for i in range(len(text)):
+       for j in range(i+1, len(text)+1):
+           yield text[i: j]
+
+
+if __name__ == '__main__':
+    a = all_variants("abc")
+    for i in a:
+        print(i)
